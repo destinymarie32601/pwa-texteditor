b72e088e7895729c2ae661bc788daacb3c4792db
destiny32601 <destiny32601@gmail.com>
2023-07-08 19:35:40 -0400

    did the TODOs in the webpack config, service worker, and the js file, created a dist folder, ran the build and npm start, installed the application

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,44 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the databse');
+
+  //creates connection to db
+  const jateDb = await openDB('jate', 1);
+
+  //create transaction 
+  const tx = jateDb.transaction(['jate'], 'readwrite');
+
+  //open the object store
+  const store = tx.objectStore('jate');
+
+  const request = store.add({ content});
+
+  const resulte = await request;
+  console.log('Data saved to the database', 'result')
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  //connection to db
+  const jateDb = await openDB('jate', 1);
+
+  //new transaction
+  const tx = jateDb.transaction(['jate'], 'readonly');
+
+  //open object store
+  const store = tx.objectStore('jate');
+
+  //get all data
+  const request = store.get(1);
+
+  //confirm request
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,65 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      databse: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
+    plugins: [ //webpack plugin to inject bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+        //injects custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      //create a manifest.json file
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Take notes with Javascript syntax highlighting!',
+        theme_color: '#225ca3',
+        background_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets','icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
-      rules: [
-        
+      rules: [ 
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }, 
       ],
     },
   };
